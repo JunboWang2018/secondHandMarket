4b9904b7abc7b9bcc5b29d2e6ecd506aa0e32ceb
wangjunbo <1105149216@qq.com>
2019-04-30 16:10:21 +0800

    update

--- MQtestServer/src/main/java/com/test/SMQdemo/TestPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.test.SMQdemo;
-
-/**
- * @author Wang Junbo
- * @description
- * @date 2019/4/25
- */
-public class TestPublisher {
-}
--- testCometD/src/main/java/com/test/service/ICometd.java
+++
@@ -1,5 +0,0 @@
-package com.test.service;
-
-public interface ICometd {
-    void call(Object cometdMessage, String topic);
-}
